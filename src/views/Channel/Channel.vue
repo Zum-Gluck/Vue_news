d15cd294ded558f42553af4c28738b077ee3c48a
JavaScriptUser <3300739800@qq.com>
2020-12-06 22:33:23 +0800

    完成CateGory

--- src/views/Channel/Channel.vue
+++ src/views/Channel/Channel.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
+  <div class="main">
     <ChannelTabControl></ChannelTabControl>
+    <ChannelContent></ChannelContent>
   </div>
 </template>
 
 <script>
 import ChannelTabControl from "./Child/ChannelTabControl";
+import ChannelContent from "./Child/ChannelContent";
 export default {
   name: "Channel",
   components: {
     ChannelTabControl,
+    ChannelContent,
   },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.main {
+  height: calc(100vh - 44px - 6px);
+  background-color: #f8f8f8;
+}
 </style>
---
+++ src/views/Channel/Child/ChannelContent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="Content">
+    <div class="delete">点击删除以下频道</div>
+    <ul class="Content_main">
+      <li v-for="item in cateList" :key="item.id">{{ item.cateName }}</li>
+    </ul>
+    <div class="delete">点击添加以下频道</div>
+    <ul class="Content_main">
+      <li>旅游</li>
+      <li>历史</li>
+      <li>探索</li>
+      <li>美食</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChannelContent",
+  data() {
+    return {
+      cateList: [],
+    };
+  },
+  created() {
+    this.getCateList();
+  },
+  methods: {
+    async getCateList() {
+      let { data: res } = await this.$http.get("/cate");
+      this.cateList = res.result;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.Content {
+  margin-top: 50px;
+  padding: 0 10px;
+  .delete {
+    color: #666666;
+  }
+}
+.Content_main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 13px;
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    width: 64px;
+    height: 34px;
+    margin-bottom: 8px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+  }
+}
+</style>
